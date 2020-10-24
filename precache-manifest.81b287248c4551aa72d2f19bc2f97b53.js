04b17c73fa85603668f1249d6830704de6d21832
Vipin Vijayvargiya <vijayvargiyav@hcl.com>
2020-10-24 20:07:24 +0200

    deploy commit

---
+++ precache-manifest.81b287248c4551aa72d2f19bc2f97b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e8665dc9b72c64436104549fcd1020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c76e0a2232f92f2bbe9",
+    "url": "/static/css/main.33bfd999.chunk.css"
+  },
+  {
+    "revision": "58106064573c3115b32e",
+    "url": "/static/js/2.7e6237d5.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/static/js/2.7e6237d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c76e0a2232f92f2bbe9",
+    "url": "/static/js/main.f918e90d.chunk.js"
+  },
+  {
+    "revision": "3f977af8f8c007b0c910",
+    "url": "/static/js/runtime-main.10a5adf0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa3dc2c175db8d2c5575663c6b58b94d.js"
+  "/precache-manifest.81b287248c4551aa72d2f19bc2f97b53.js"
 );
 
 self.addEventListener('message', (event) => {
