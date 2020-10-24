8586dd8bb59f830680875534e20a23098c537df5
Vipin Vijayvargiya <vijayvargiyav@hcl.com>
2020-10-24 19:34:52 +0200

    deploy commit

---
+++ precache-manifest.501ae51173ddc6f35f816dfe7cb635e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2ddff1403ea579276a0c98b183d332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90dd37193be79c428a33",
+    "url": "/static/css/main.d8541390.chunk.css"
+  },
+  {
+    "revision": "12e03245820c719e5c65",
+    "url": "/static/js/2.a6396241.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/static/js/2.a6396241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dd37193be79c428a33",
+    "url": "/static/js/main.1969b8b9.chunk.js"
+  },
+  {
+    "revision": "3f977af8f8c007b0c910",
+    "url": "/static/js/runtime-main.10a5adf0.js"
+  }
+]);
